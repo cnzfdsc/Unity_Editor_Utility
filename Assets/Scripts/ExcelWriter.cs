27638701a1ce44d36394b57d6f70c45ec3c49bb3
cnzfdsc <cnzfdsc@163.com>
2019-04-20 16:50:58 +0800

    写Excel

---
+++ Assets/Scripts/ExcelWriter.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using Excel;
+using System.Data;
+using System.IO;
+using System.Collections.Generic;
+using OfficeOpenXml;
+using UnityEditor;
+
+
+public class ExcelWriter
+{
+	private ExcelPackage m_ExcelPackage;
+	private ExcelWorksheet m_WorkSheet;
+	private int m_ColumnCount;
+
+	/// <summary>
+	/// 写入 Excel ; 需要添加 OfficeOpenXml.dll;
+	/// </summary>
+	public void OpenExcel(string excelPath)
+	{
+		//通过面板设置excel路径
+		//string outputDir = EditorUtility.SaveFilePanel("Save Excel", "", "New Resource", "xlsx");
+		
+		//string path = Application.dataPath + "/" + excelPath;
+		FileInfo newFile = new FileInfo(excelPath);
+
+		if (!newFile.Exists)
+		{
+			newFile = new FileInfo(excelPath);
+		}
+		
+		m_ExcelPackage = new ExcelPackage(newFile);
+	}
+
+	public ExcelWorksheet OpenSheet(string sheetName)
+	{
+		if (!ExistSheet(sheetName))
+		{
+			m_WorkSheet = m_ExcelPackage.Workbook.Worksheets.Add(sheetName);
+		}
+		else
+		{
+			m_WorkSheet = m_ExcelPackage.Workbook.Worksheets[sheetName];
+		}
+
+		return m_WorkSheet;
+	}
+
+	public void SetExcelHeader(string[] headColumns)
+	{
+		m_ColumnCount = headColumns.Length;
+
+		for (int iCol = 0; iCol < headColumns.Length; iCol++)
+		{
+			m_WorkSheet.Cells[1, iCol+1].Value = headColumns[iCol];
+		}
+	}
+
+	/// <summary>
+	/// 强制写一个格子的数据
+	/// </summary>
+	/// <param name="row"></param>
+	/// <param name="column"></param>
+	/// <param name="value"></param>
+	public void WriteExcel(int row, int column, string value)
+	{
+		m_WorkSheet.Cells[row, column].Value = value;
+
+		//m_WorkSheet.Cells[1, 1].Value = "ID";
+	}
+
+	public void WriteRow(int row, string[] values)
+	{
+		if (m_ColumnCount == 0)
+		{
+			Debug.LogErrorFormat("还没设置表头");
+			return;
+		}
+
+		if (values.Length != m_ColumnCount)
+		{
+			Debug.LogErrorFormat("列数与表头不匹配");
+		}
+
+		for (int iCol = 0; iCol < values.Length; iCol++)
+		{
+			m_WorkSheet.Cells[row, iCol+1].Value = values[iCol];
+		}
+	}
+
+	public void SaveAndCloseExcel()
+	{
+		m_ExcelPackage.Save();
+		m_ExcelPackage.Dispose();
+	}
+
+	private bool ExistSheet(string sheetName)
+	{
+		bool exist = false;
+		for (int iSheet = 0; iSheet < m_ExcelPackage.Workbook.Worksheets.Count; iSheet++)
+		{
+			m_ExcelPackage.Workbook.Worksheets[iSheet+1].Name = sheetName;
+			exist = true;
+			break;
+		}
+
+		return exist;
+	}
+}
---
+++ Assets/Scripts/WriteExcelTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class WriteExcelTest : MonoBehaviour
+{
+	private void Start()
+	{
+		string dataPath = Application.dataPath;
+		ExcelWriter excelWriter = new ExcelWriter();
+		excelWriter.OpenExcel(Application.dataPath + "ExcelTest.xlsx");
+		excelWriter.OpenSheet("Launchers");
+		excelWriter.SetExcelHeader(new string[] { "ShipName", "Launchers" });
+		List<Vector3> vList = new List<Vector3>();
+		vList.Add(Vector3.zero);
+		vList.Add(Vector3.forward);
+		vList.Add(Vector3.one);
+		excelWriter.WriteRow(2, new string[] { "Avatar", VectorListToString(vList) });
+		excelWriter.SaveAndCloseExcel();
+	}
+
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+		{
+		}
+
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+
+		}
+	}
+
+	private string VectorListToString(List<Vector3> vList)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (int iVector = 0; iVector < vList.Count; iVector++)
+		{
+			sb.Append(vList[iVector].x);
+			sb.Append(",");
+			sb.Append(vList[iVector].y);
+			sb.Append(",");
+			sb.Append(vList[iVector].z);
+			if (iVector < vList.Count - 1)
+				sb.Append("|");
+		}
+
+		return sb.ToString();
+	}
+}
